7484fb8bcc437dbd66212b89038947d146be4d26
Diyorbek-Nabijonov <baxrom3990@gmail.com>
2021-10-02 14:05:14 +0300

    first commit

---
+++ src/routes/messenger/Messenger.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Messenger() {
+    return (
+        <div className="messenger">
+        </div>
+    )
+}
+
+export default Messenger
---
+++ src/routes/profile-page/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../components/header/Header'
+import './Profile.css'
+function Profile() {
+    return (
+        <div className="profile">
+                <Header/>
+                <h1>This is Profile</h1>
+        </div>
+    )
+}
+
+export default Profile
